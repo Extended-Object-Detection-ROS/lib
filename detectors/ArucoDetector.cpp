806b9a11dfcc6bb86d7245748b8eaca95f54a5d0
MoscowskyAnton <moscowskyad@gmail.com>
2021-11-01 07:30:34 -0400

    remove global detector from aruco

--- detectors/ArucoDetector.cpp
+++ detectors/ArucoDetector.cpp
@@ -5,45 +5,29 @@
 using namespace std;
 using namespace cv;
 
-namespace eod{
-    
-    map <cv::aruco::PREDEFINED_DICTIONARY_NAME, GlobalArucoDetector*> GADS;
+namespace eod{    
         
-    ArucoAttribute::ArucoAttribute(){
-        this->IDmarker = -1;
+    ArucoAttribute::ArucoAttribute(){     
         Type = ARUCO_A;        
         inited = false;
     }
     
-    ArucoAttribute::ArucoAttribute(int dictionary, int IDmarker_, double markerLen_){
-        if( dictionary < 0 ){
+    ArucoAttribute::ArucoAttribute(int dictionary_, int IDmarker_, double markerLen_){
+        if( dictionary_ < 0 ){
             dict = cv::aruco::DICT_4X4_50;
             printf("Aruco dictionary didin't suport negative values, dictionary is set to  default 4x4_50\n");
         }
-        else if( dictionary >  20){
+        else if( dictionary_ >  20){
             dict = cv::aruco::DICT_4X4_50;
             printf("Aruco dictionary cant be more than 20, dictionary is set to  default 4x4_50\n");
         }
         else
-            dict = cv::aruco::PREDEFINED_DICTIONARY_NAME(dictionary);
+            dict = cv::aruco::PREDEFINED_DICTIONARY_NAME(dictionary_);
         
         markerLen = markerLen_;
-        
-        this->IDmarker = IDmarker_ >= 0 ? IDmarker_ : -1;
-        Type = ARUCO_A;        
-        
-        map<cv::aruco::PREDEFINED_DICTIONARY_NAME,GlobalArucoDetector*>::iterator it;
-        it = GADS.find(dict);
-        if ( it == GADS.end() ) {
-            GAD = new GlobalArucoDetector(dict);
-            //printf("try to set this\n");
-            //GAD->setCamParams(parent_base->getCameraMatrix(), parent_base->getDistortionCoeff() );
-            //printf("set\n");
-            GADS.insert(pair<cv::aruco::PREDEFINED_DICTIONARY_NAME,GlobalArucoDetector*>(dict,GAD));            
-        }
-        else{
-            GAD = (*it).second;
-        }
+                
+        Type = ARUCO_A;                        
+        dictionary = cv::aruco::getPredefinedDictionary(dict);        
         inited = true;        
     }    
     
@@ -51,11 +35,32 @@ namespace eod{
         if( !inited )
             return vector<ExtendedObjectInfo>(0);
         
-        if( !GAD->hasCamParams() ){
-            GAD->setCamParams(parent_base->getCameraMatrix(), parent_base->getDistortionCoeff() );
+        if( hasCamParams() ){
+            setCamParams(parent_base->getCameraMatrix(), parent_base->getDistortionCoeff() );
         }
-                
-        return GAD->detect(image, IDmarker, seq, markerLen, Weight, returnContours);
+                        
+        vector<ExtendedObjectInfo> rects;        
+        if( seq == 0 || seq != prev_seq ){            
+            cv::aruco::detectMarkers(image, dictionary, markerCorners, markerIds);
+        }        
+        for( size_t i = 0 ; i < markerIds.size(); i++ ){            
+            ExtendedObjectInfo tmp = boundingRect( markerCorners[i] );                                                                  
+            set_extracted_info(tmp, "marker_id", markerIds[i]);            
+            if( returnContours )
+                tmp.contour.push_back(float2intPointVector(markerCorners[i]));
+            if( hasCamParams() && markerLen > 0){
+                vector<cv::Vec3d> rvecs, tvecs;                    
+                cv::aruco::estimatePoseSingleMarkers(vector<vector<Point2f> >(markerCorners.begin()+i,markerCorners.begin()+i+1), markerLen, camMat, distCoef, rvecs, tvecs);
+                tmp.tvec.push_back(tvecs[0]);
+                tmp.rvec.push_back(rvecs[0]);
+            }
+            tmp.setScoreWeight(1, Weight);//TODO use reprojection to calc 
+            rects.push_back(tmp);                
+            
+        }
+        if( seq != 0)
+            prev_seq = seq;
+        return rects;       
     }
           
         
@@ -66,48 +71,12 @@ namespace eod{
     void ArucoAttribute::Extract2(const cv::Mat& image, ExtendedObjectInfo& rect){
     }
     
-    /// GLOBAL DETECTORS
-    GlobalArucoDetector::GlobalArucoDetector(cv::aruco::PREDEFINED_DICTIONARY_NAME dict){
-        dictionary = cv::aruco::getPredefinedDictionary(dict);
-    }
-    
-    vector<ExtendedObjectInfo> GlobalArucoDetector::detect(const Mat& image, int IDmarker, double seq, double markerLen, double Weight, bool returnContours){
-        vector<ExtendedObjectInfo> rects;        
-        if( seq == 0 || seq != prev_seq ){
-            //markerIds.clear();
-            //markerCorners.clear();
-            cv::aruco::detectMarkers(image, dictionary, markerCorners, markerIds);
-        }        
-        for( size_t i = 0 ; i < markerIds.size(); i++ ){
-            if( IDmarker == -1 || IDmarker == markerIds[i] ){
-                ExtendedObjectInfo tmp = boundingRect( markerCorners[i] );                                                
-                //tmp.sub_id[0] = markerIds[i];
-                //tmp.extracted_info[0] = to_string(markerIds[i]);       
-                //set_extracted_info(&tmp, 'marker_id', markerIds[i]);
-                tmp.extracted_info["Aruco:marker_id"] = std::to_string(markerIds[i]);
-                if( returnContours )
-                    tmp.contour.push_back(float2intPointVector(markerCorners[i]));
-                if( hasCamParams() && markerLen > 0){
-                    vector<cv::Vec3d> rvecs, tvecs;                    
-                    cv::aruco::estimatePoseSingleMarkers(vector<vector<Point2f> >(markerCorners.begin()+i,markerCorners.begin()+i+1), markerLen, camMat, distCoef, rvecs, tvecs);
-                    tmp.tvec.push_back(tvecs[0]);
-                    tmp.rvec.push_back(rvecs[0]);
-                }
-                tmp.setScoreWeight(1, Weight);//TODO use reprojection to calc 
-                rects.push_back(tmp);                
-            }
-        }
-        if( seq != 0)
-            prev_seq = seq;
-        return rects;        
-    }
-    
-    void GlobalArucoDetector::setCamParams(Mat camMat_, Mat distCoef_){
+    void ArucoAttribute::setCamParams(Mat camMat_, Mat distCoef_){
         camMat = camMat_;
         distCoef = distCoef_;
     }
     
-    bool GlobalArucoDetector::hasCamParams(){
+    bool ArucoAttribute::hasCamParams(){
         return !(camMat.empty() & distCoef.empty());
     }
     
--- detectors/include/ArucoDetector.h
+++ detectors/include/ArucoDetector.h
@@ -8,28 +8,6 @@
 
 namespace eod{        
     
-    class GlobalArucoDetector{
-    public:
-        GlobalArucoDetector(cv::aruco::PREDEFINED_DICTIONARY_NAME dict);
-        
-        std::vector<ExtendedObjectInfo> detect(const cv::Mat& image, int ID, double seq, double markerLen = 0, double Weight = 1, bool returnContours = true);
-        
-        std::vector< int > markerIds; 
-        std::vector< std::vector<cv::Point2f> > markerCorners;
-        
-        void setCamParams(cv::Mat camMat, cv::Mat distCoef);        
-        bool hasCamParams();
-        
-    private:
-        cv::Ptr<cv::aruco::Dictionary> dictionary;    
-        double prev_seq;
-        cv::Mat camMat;
-        cv::Mat distCoef;
-        
-    };
-    
-    extern std::map <cv::aruco::PREDEFINED_DICTIONARY_NAME, GlobalArucoDetector*> GADS;
-    
     class ArucoAttribute : public Attribute{
     public:	    
         /// <summary>
@@ -48,25 +26,26 @@ namespace eod{
         /// <param name="image">Destination image</param>
         /// <returns>Vector of rects of found objects</returns>
         std::vector<ExtendedObjectInfo> Detect2(const cv::Mat& image, int seq);
-
-        /*
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="image">Destination image</param>
-        /// <returns>Vector of rects of appropriate objects</returns>
-        vector<ExtendedObjectInfo> Check(Mat image, vector<ExtendedObjectInfo> rects);
-        */
+        
         bool Check2(const cv::Mat& image, ExtendedObjectInfo& rect);
         
         void Extract2(const cv::Mat& image, ExtendedObjectInfo& rect);
+        
+        void setCamParams(cv::Mat camMat, cv::Mat distCoef);        
+        bool hasCamParams();
+        
+        
     private:
+                        
+        cv::aruco::PREDEFINED_DICTIONARY_NAME dict;                
+        cv::Ptr<cv::aruco::Dictionary> dictionary;
+                
+        cv::Mat camMat;
+        cv::Mat distCoef;        
         
-        // ID = -1 means detector search all aruco markers
-        int IDmarker;
+        std::vector< std::vector<cv::Point2f> > markerCorners;
+        std::vector< int > markerIds; 
         
-        cv::aruco::PREDEFINED_DICTIONARY_NAME dict;
-        GlobalArucoDetector *GAD;                           
         double markerLen;
     };
 
