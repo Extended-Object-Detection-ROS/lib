d7806591ea6e9129836a557854a088856be09487
moscowsky <moscowskyad@gmail.com>
2023-06-06 14:34:04 +0300

    data is obtained but problem with time

--- detectors/ROSSubscriberOpenPoseRaw.cpp
+++ detectors/ROSSubscriberOpenPoseRaw.cpp
@@ -11,19 +11,20 @@ namespace eod{
     
     void ROSSubscriberOpenPoseRaw::callback(const depthai_ros_extended_msgs::NeuralNetworkRawOutput::ConstPtr& msg){
         printf("Got msg!\n");
-        //last_msg = *msg;
+        last_msg = *msg;
     }
     
     std::vector<ExtendedObjectInfo> ROSSubscriberOpenPoseRaw::Detect2(const InfoImage& image, int seq){
         std::vector<ExtendedObjectInfo> results;
         
         depthai_ros_extended_msgs::NeuralNetworkRawOutput msg_copy = last_msg;
-        if( fabs(msg_copy.header.stamp.toSec() - image.timestamp() ) > timelag_){
-            printf("Message was skipped due old frame\n");
+        float lag = fabs(msg_copy.header.stamp.toSec() - image.timestamp() );
+        if( lag > timelag_){
+            printf("Message was skipped due old frame (lag = %.3f)\n", lag);
             return results;
         }
         if( msg_copy.header.frame_id != last_msg.header.frame_id ){
-            printf("Message was skipped due other frame_id\n");
+            printf("Message was skipped due other frame_id (%s)\n", msg_copy.header.frame_id.c_str());
             return results;
         }
         printf("Got msg\n");
